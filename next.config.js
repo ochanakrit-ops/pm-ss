a3805299767771a50d364b5679aea6712b16a379
ochanakrit-ops <ochanakrit@gmail.com>
2026-02-24 15:37:29 +0700

    fix port 10000

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverMinification: false,
+  },
   reactStrictMode: true,
   poweredByHeader: false,
 };
