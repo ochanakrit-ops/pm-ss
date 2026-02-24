b8d039691cf7f814c501def041a4243495ecf038
ochanakrit-ops <ochanakrit@gmail.com>
2026-02-24 22:12:52 +0700

    1.6.2 Fix certificate chain-02

--- lib/db.ts
+++ lib/db.ts
@@ -7,18 +7,11 @@ export function getPool() {
   if (!pool) {
     assertEnv();
     pool = new Pool({
-                    connectionString: env.DATABASE_URL,
-                    ssl: {
-                      rejectUnauthorized: false,
-                    },
-                    max: 10,
-                  });
+      connectionString: env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
   }
   return pool;
-}
-
-export async function q<T = any>(text: string, params?: any[]): Promise<T[]> {
-  const p = getPool();
-  const res = await p.query(text, params);
-  return res.rows as T[];
 }
